3d3898893ae212d25cd455cd43eacfb45c857b4a
Tiago Silva <tiagosp13@gmail.com>
2022-11-12 18:12:49 -0300

    init

---
+++ src/main/java/com/security/oauth2withmfa/Oauth2WithMfaApplication.java
@@ -0,0 +1,13 @@
+package com.security.oauth2withmfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2WithMfaApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2WithMfaApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/security/oauth2withmfa/Oauth2WithMfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.oauth2withmfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2WithMfaApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
